ada3014bfc563566b65be0f7df32cb177ee8cb12
Terra-Soul <earthenchronicler@gmail.com>
2017-11-29 13:31:52 -0500

    Update MarinaDatabase.java

--- MarinaDatabase.java
+++ MarinaDatabase.java
@@ -9,6 +9,11 @@ public class MarinaDatabase {
     PreparedStatement searchCustByName = null;
     PreparedStatement findPersonByLName = null;
     PreparedStatement addNewCust = null;
+    PreparedStatement addNewPowerBoat = null;
+    PreparedStatement addNewSailBoat = null;
+    PreparedStatement addNewLease = null;
+    PreparedStatement addNewMaintenance = null;
+    
 
     public MarinaDatabase(){
         try{
@@ -57,61 +62,75 @@ public class MarinaDatabase {
         }
     }
     
-    public void addNewPowerBoat(int license, String fName, String lName){
+    public void addNewPowerBoat(int registrationNum, int size, String fuelType, int numEngines, String engineType){
         try{
-            addNewCust = connection.prepareStatement("INSERT INTO CUSTOMER(boatingLicense, firstName, lastName) VALUES(?, ?, ?)");
+            addNewPowerBoat = connection.prepareStatement("INSERT INTO POWERBOAT(registrationNum, size, fuelType, numEngines, engineType) VALUES(?, ?, ?, ?, ?)");
 
-            addNewCust.setInt(1, license);
-            addNewCust.setString(2, fName);
-            addNewCust.setString(3, lName);
+            addNewPowerBoat.setInt(1, registrationNum);
+            addNewPowerBoat.setInt(2, size);
+            addNewPowerBoat.setString(3, fuelType);
+            addNewPowerBoat.setInt(4, numEngines);
+            addNewPowerBoat.setString(5, engineType);
+           
 
-            System.out.println("Customer added!");
-            int ans = addNewCust.executeUpdate();
+            System.out.println("Power Boat added!");
+            int ans = addNewPowerBoat.executeUpdate();
         }
         catch(Exception e){
             JOptionPane.showMessageDialog(null, e.getMessage());
         }
     }
-    public void addNewCust(int license, String fName, String lName){
+    public void addNewSailBoat(int registrationNum, int size, String fuelType, int keelHeight, boolean hasEngine, int sailNum){
         try{
-            addNewCust = connection.prepareStatement("INSERT INTO CUSTOMER(boatingLicense, firstName, lastName) VALUES(?, ?, ?)");
+            addNewSailBoat = connection.prepareStatement("INSERT INTO SAILBOAT(registrationNum, size, fuelType, keelHeight, hasEngine, sailNum) VALUES(?, ?, ?, ?, ?, ?)");
 
-            addNewCust.setInt(1, license);
-            addNewCust.setString(2, fName);
-            addNewCust.setString(3, lName);
+            addNewSailBoat.setInt(1, registrationNum);
+            addNewSailBoatt.setInt(2, size);
+            addNewSailBoat.setString(3, fuelType);
+            addNewSailBoat.setInt(4, keelHeight);
+            addNewSailBoat.setBoolean(5, hasEngine);
+            addNewSailBoat.setInt(6, sailNum);
+           
 
-            System.out.println("Customer added!");
-            int ans = addNewCust.executeUpdate();
+            System.out.println("Sail Boat added!");
+            int ans = addNewSailBoat.executeUpdate();
         }
         catch(Exception e){
             JOptionPane.showMessageDialog(null, e.getMessage());
         }
     }
-    public void addNewCust(int license, String fName, String lName){
+    public void addNewLease(int slipNumber, int slotNumber, int duration, double rate, String expirationDate){
         try{
-            addNewCust = connection.prepareStatement("INSERT INTO CUSTOMER(boatingLicense, firstName, lastName) VALUES(?, ?, ?)");
+            addNewLease = connection.prepareStatement("INSERT INTO LEASE(slipNumber, slotNumber, duration, rate, expirationDate) VALUES(?, ?, ?, ?, ?)");
 
-            addNewCust.setInt(1, license);
-            addNewCust.setString(2, fName);
-            addNewCust.setString(3, lName);
-
-            System.out.println("Customer added!");
-            int ans = addNewCust.executeUpdate();
+            addNewLease.setInt(1, slotNumber);
+            addNewLease.setInt(1, slotNumber);
+            addNewLease.setInt(1, duration);
+            addNewLease.setDouble(2, rate);
+            addNewLease.setString(3, expirationDate);
+            
+            System.out.println("Lease Created!");
+            int ans = addNewLease.executeUpdate();
+            
+            //possible location for boatLeases and customerLeases code
+            
         }
         catch(Exception e){
             JOptionPane.showMessageDialog(null, e.getMessage());
         }
     }
-    public void addNewCust(int license, String fName, String lName){
+    public void addNewMaintenance(int invoiceNum, String dateOfMaintenance, String workDone){
         try{
-            addNewCust = connection.prepareStatement("INSERT INTO CUSTOMER(boatingLicense, firstName, lastName) VALUES(?, ?, ?)");
+            addNewMaintenance = connection.prepareStatement("INSERT INTO MAINTENANCE(invoiceNum, dateOfMaintenance, workDone) VALUES(?, ?, ?)");
 
-            addNewCust.setInt(1, license);
-            addNewCust.setString(2, fName);
-            addNewCust.setString(3, lName);
+            addNewMaintenance.setInt(1, invoiceNum);
+            addNewMaintenance.setString(2, dateOfMaintenance);
+            addNewMaintenance.setString(3, workDone);
 
-            System.out.println("Customer added!");
-            int ans = addNewCust.executeUpdate();
+            System.out.println("Maintenance record created!");
+            int ans = addNewMaintenance.executeUpdate();
+            
+            //possible location for boatMaintenance code
         }
         catch(Exception e){
             JOptionPane.showMessageDialog(null, e.getMessage());
